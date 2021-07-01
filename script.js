868cb13a0976dc0a90279542612574feff45909d
Priya Ghosh <41378468+priyaghosh1234@users.noreply.github.com>
2021-07-01 18:20:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value;
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
